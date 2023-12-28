6e08822006c969b23f15bdcb07773bf31e08742b
Mokhinur97 <154824553+Mokhinur97@users.noreply.github.com>
2023-12-28 21:49:30 +0700

    Add files via upload

---
+++ File1.py
@@ -0,0 +1,23 @@
+t = float(input())
+if t < 15.5:
+    print(" ХОЛОДНО ")
+elif t > 28:
+    print(" ЖАРКО ")
+else:
+    print(" НОРМАЛЬНО ")
+
+
+
+
+x = int(input())
+i = 1
+while i <= x:
+    i += 1
+    y = input()
+    if "Кот" in y or "кот" in y:
+        t = true
+        continue
+    if t == True:
+        print(" МЯУ ")
+    else:
+        print(" НЕТ ")
